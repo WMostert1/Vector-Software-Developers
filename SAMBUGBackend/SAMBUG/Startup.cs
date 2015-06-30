f258cad34ea7c8b306262705adfdff68cc005bc6
Werner Mostert <werner.mostert1@gmail.com>
2015-06-30 23:19:29 +0200

    Added original projects for the android client and backend
    I'm creating these to see if the automated builds actually work

---
+++ SAMBUGBackend/SAMBUG/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SAMBUG.Startup))]
+namespace SAMBUG
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
