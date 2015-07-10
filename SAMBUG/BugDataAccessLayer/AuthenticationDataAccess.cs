867cfb1dd47b95ff743d66e47bee36829537ba79
Abrie van Aardt <linkin1903@gmail.com>
2015-07-10 12:20:31 +0200

    added data access layer

---
+++ SAMBUG/BugDataAccessLayer/AuthenticationDataAccess.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugDataAccessLayer
+{
+    class AuthenticationDataAccess
+    {
+        private BugDBEntities _dbContext = new BugDBEntities();
+
+        public bool Login(string username, string password)
+        {
+            return false;
+        }
+    }
+}
