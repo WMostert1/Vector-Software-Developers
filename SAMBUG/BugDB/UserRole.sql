325aa3e26b9b2800ef5d96b2b27c7ada3d07d106
Abrie van Aardt <linkin1903@gmail.com>
2015-07-13 15:37:14 +0200

    added UserRole link table allowing for multiple roles

---
+++ SAMBUG/BugDB/UserRole.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[UserRole]
+(
+	[UserRoleID] INT NOT NULL PRIMARY KEY DEFAULT NEXT VALUE FOR [dbo].[UserRole_UserRoleID_Sequence],
+	[UserID] INT NOT NULL,
+	[RoleID] INT NOT NULL,
+	CONSTRAINT [FK_UserRole_ToUser] FOREIGN KEY ([UserID]) REFERENCES [User]([UserID]),
+	CONSTRAINT [FK_UserRole_ToRole] FOREIGN KEY ([RoleID]) REFERENCES [Role]([RoleID])
+)
