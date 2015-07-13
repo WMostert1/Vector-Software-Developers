8304e2abaa4b14bf36308046049114c24bbffd59
Keagan Thompson <u13023782@tuks.co.za>
2015-07-13 13:24:15 +0200

    Refactored scoutdataactivity

--- SAMBUGApp/app/src/main/java/vsd/co/za/sambugapp/enterDataActivity.java
+++ SAMBUGApp/app/src/main/java/vsd/co/za/sambugapp/enterDataActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.location.LocationManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
