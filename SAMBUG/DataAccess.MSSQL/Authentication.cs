2ba4e040337f68658d064beb4d20543746c123a5
Michelle14 <michelle4swanepoel@gmail.com>
2015-07-13 12:27:52 +0200

    Added authentication files

---
+++ SAMBUG/DataAccess.MSSQL/Authentication.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess.Interface;
+
+namespace DataAccess.MSSQL
+{
+    public class Authentication : IAuthentication
+    {
+    }
+}
