b4f39e6e1a28b6dcc2234d30ecfc3e3200c9cbdf
KaleabTessera <kaleabtessera@gmail.com>
2015-08-18 23:51:50 +0200

    Worked on sync service.

---
+++ SAMBUG/DataAccess.Interface/IDbSynchronization.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess.Interface.Domain;
+
+namespace DataAccess.Interface
+{
+    class IDbSynchronization
+    {
+        bool PersistBugStops(Int64 scoutStopID, Int64 userID, Int64 blockID, int numberOfTrees, float latitude, float longitude, DateTime datetime, int lastModifiedID, DateTime tmStamp);
+        bool PersistScoutBugs(Int64 scoutBugID, Int64 scoutStopID, Int64 speciesID, int NumberOfBugs, byte[] image, string comments, int lastModifiedID, DateTime tmpStamp);
+    }
+}
---
+++ SAMBUG/DataAccess.MSSQL/DbSynchronization.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess.Interface;
+
+namespace DataAccess.MSSQL
+{
+    class DbSynchronization: IDbAuthentication
+    {
+       
+        public bool PersistBugStops(Int64 scoutStopID, Int64 userID, Int64 blockID, int numberOfTrees, float latitude, float longitude, DateTime datetime, int lastModifiedID, DateTime tmStamp)
+        {
+            bool success = false;
+            var db = new BugDBEntities();
+            return success;
+        }
+
+        public bool PersistScoutBugs(Int64 scoutBugID, Int64 scoutStopID, Int64 speciesID, int NumberOfBugs, byte[] image, string comments, int lastModifiedID, DateTime tmpStamp)
+        {
+            bool success = false;
+            var db = new BugDBEntities();
+            return success;
+
+        }
+    }
+}
