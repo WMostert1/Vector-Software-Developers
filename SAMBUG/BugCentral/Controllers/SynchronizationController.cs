5abd2e571a5d7365d018c8c1201e43d4fcfedb52
KaleabTessera <kaleabtessera@gmail.com>
2015-08-20 12:04:19 +0200

    Added features to synchronization.

---
+++ SAMBUG/BugCentral/Controllers/SynchronizationController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+using System.Web.Http.Results;
+using BugBusiness.Interface.BugSecurity;
+using BugBusiness.Interface.BugSecurity.DTO;
+using BugBusiness.Interface.BugSecurity.Exceptions;
+
+namespace BugCentral.Controllers
+{
+    [RoutePrefix("api/synchronization")]
+    public class SynchronizationController
+    {
+        public SynchronizationController()
+        {
+            _bugSecurity = bugSecurity;
+        }
+    }
+}
