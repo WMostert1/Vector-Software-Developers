c0fedc41c0b01a5d039bd227892ebd730e372d88
KaleabTessera <kaleabtessera@gmail.com>
2015-08-22 15:53:06 +0200

    Format of timestamp variable was accounted for.

--- SAMBUG/BugCentral/Controllers/SynchronizationController.cs
+++ SAMBUG/BugCentral/Controllers/SynchronizationController.cs
@@ -10,21 +10,26 @@ using BugBusiness.Interface.BugSecurity.Exceptions;
 using Newtonsoft.Json.Linq;
 using DataAccess.Interface;
 
+using System.Web;
+using System.Web.Mvc;
+using System.Text;
+
 namespace BugCentral.Controllers
 {
-    [RoutePrefix("api/synchronization")]
-    public class SynchronizationController : ApiController
+    [RoutePrefix("Synchronization")]
+    public class SynchronizationController : Controller
     {
         private IDbSynchronization dbSynchronization { get; set; }
         private string json;
-
-        public SynchronizationController(IDbSynchronization _dbSynchronization, String _json)
+    
+        public SynchronizationController(IDbSynchronization _dbSynchronization ) //, String _json)
         {
             dbSynchronization = _dbSynchronization;
-            json = _json;
+           // json = _json;
         }
 
-        private Boolean sync(string json)
+         [Route("sync")]
+        public Boolean sync(string json)
         {
             try
             {
@@ -38,13 +43,13 @@ namespace BugCentral.Controllers
                     Int64 userID = Convert.ToInt64(scoutStop["UserID"].ToString());
                     Int64 blockID = Convert.ToInt64(scoutStop["BlockID"].ToString());
                     int numberOfTrees = Convert.ToInt16(scoutStop["NumberOfTrees"].ToString());
-                    double latitude = Convert.ToInt32(scoutStop["Latitude"].ToString());
-                    double longitude = Convert.ToInt32(scoutStop["Longitude"].ToString());
+                    float latitude = float.Parse(scoutStop["Latitude"].ToString().Replace(".", ","));
+                    float longitude = float.Parse(scoutStop["Longitude"].ToString().Replace(".", ","));
                     DateTime date = Convert.ToDateTime(scoutStop["Date"].ToString());
                     int lastModifiedID = Convert.ToInt16(scoutStop["LastModifiedID"].ToString());
-                    string tmStamp = scoutStop["TMStamp"].ToString();
+                    DateTime tmStamp = Convert.ToDateTime(long.Parse(scoutStop["TMStamp"].ToString()));
 
-                   // dbSynchronization.PersistBugStops(scoutStopID, userID, blockID, numberOfTrees, latitude, longitude, date, lastModifiedID, tmStamp);
+                    dbSynchronization.PersistBugStops(scoutStopID, userID, blockID, numberOfTrees, latitude, longitude, date, lastModifiedID, tmStamp);
                 }
 
                 foreach (var scoutBugs in actualData["ScoutBugs"])
@@ -53,12 +58,12 @@ namespace BugCentral.Controllers
                     Int64 scoutStopID = Convert.ToInt64(scoutBugs["ScoutStopID"].ToString());
                     Int64 speciesID = Convert.ToInt64(scoutBugs["SpeciesID"].ToString());
                     int numberOfBugs = Convert.ToInt16(scoutBugs["NumberOfBugs"].ToString());
-                    String fieldPicture = scoutBugs["FieldPicture"].ToString();
+                    byte[] fieldPicture = Encoding.ASCII.GetBytes(scoutBugs["FieldPicture"].ToString());
                     String comments = scoutBugs["Comments"].ToString();
                     int lastModifiedID = Convert.ToInt16(scoutBugs["LastModifiedID"].ToString());
-                    String tmStamp = scoutBugs["TMStamp"].ToString();
+                    DateTime tmStamp = Convert.ToDateTime(long.Parse(scoutBugs["TMStamp"].ToString()));
 
-                 //   dbSynchronization.PersistScoutBugs(scoutBugID, scoutStopID, speciesID, numberOfBugs, fieldPicture, comments, lastModifiedID, tmStamp);
+                    dbSynchronization.PersistScoutBugs(scoutBugID, scoutStopID, speciesID, numberOfBugs, fieldPicture, comments, lastModifiedID, tmStamp);
                 }
             }
             catch (Exception e)
