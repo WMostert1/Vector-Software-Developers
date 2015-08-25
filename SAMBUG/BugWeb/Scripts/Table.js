9eeb75ece19a71cfcbf1db4e3fd6da372bca3998
Michelle14 <michelle4swanepoel@gmail.com>
2015-08-25 21:25:23 +0200

    Added to tabular data view

--- SAMBUG/BugWeb/Scripts/Table.js
+++ SAMBUG/BugWeb/Scripts/Table.js
@@ -1,3 +1,29 @@
-﻿function generate() {
-    console.log("hello");
+﻿$(document).ready(function () {
+    setFromDate();
+    setToDate();
+});
+
+function generate()
+{
+/*    alert("hello");*/
+};
+
+
+function setFromDate() {
+    var date = new XDate();
+    var newDate = date.addMonths(-6, true);
+    $("#timeFrom").val(newDate.toString("yyyy-MM-dd"));
+
+};
+
+function setToDate() {
+    var date = new XDate();
+    $("#timeTo").val(date.toString("yyyy-MM-dd"));
 }
+
+
+
+
+
+
+
