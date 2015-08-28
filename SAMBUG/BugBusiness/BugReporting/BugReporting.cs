5c687f69434980829dfc0db3ca2461bb588568b5
Abrie van Aardt <linkin1903@gmail.com>
2015-08-28 17:46:23 +0200

    Finished GetCapturedData API method

--- SAMBUG/BugBusiness/BugReporting/BugReporting.cs
+++ SAMBUG/BugBusiness/BugReporting/BugReporting.cs
@@ -22,14 +22,24 @@ namespace BugBusiness.BugReporting
         public GetCapturedDataResponse GetCapturedData(GetCapturedDataRequest getCapturedDataRequest)
         {
             List<ScoutStop> scoutStops = _dbBugReporting.GetScoutStopsByFarmId(getCapturedDataRequest.FarmId);
-            List<ScoutStopDto> scoutStopsDto = AutoMapper.Mapper.Map<List<ScoutStopDto>>(scoutStops);
-            
             List<Treatment> treatments = _dbBugReporting.GetTreatmentsByFarmId(getCapturedDataRequest.FarmId);
+
+            if (!scoutStops.Any() && !treatments.Any())
+                return null;
+
+            string farmName;
+
+            if (!scoutStops.Any())
+                farmName = treatments[0].Block.Farm.FarmName;
+            else
+                farmName = scoutStops[0].Block.Farm.FarmName;
+            
+            List<ScoutStopDto> scoutStopsDto = AutoMapper.Mapper.Map<List<ScoutStopDto>>(scoutStops);
             List<TreatmentDto> treatmentsDto = AutoMapper.Mapper.Map<List<TreatmentDto>>(treatments);
 
             return new GetCapturedDataResponse()
             {
-                FarmName = scoutStops[0].Block.Farm.FarmName,
+                FarmName = farmName,
                 ScoutStops = scoutStopsDto,
                 Treatments = treatmentsDto
             };
