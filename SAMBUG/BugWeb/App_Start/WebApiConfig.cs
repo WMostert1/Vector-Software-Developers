ef56eab2ceb7e6f3ebc480c6902eca81d98d2611
Werner Mostert <werner.mostert1@gmail.com>
2015-08-31 15:04:09 +0200

    Added API route

--- SAMBUG/BugWeb/App_Start/WebApiConfig.cs
+++ SAMBUG/BugWeb/App_Start/WebApiConfig.cs
@@ -13,7 +13,7 @@ namespace BugWeb
           
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "{controller}/{id}",
+                routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
         }
