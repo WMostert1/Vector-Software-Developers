ffd772fddebec1863756ac89484050ec4e1abcc8
Keagan Thompson <u13023782@tuks.co.za>
2015-09-05 13:15:29 +0200

    Have to commit again for some reason

---
+++ SAMBUG/BugBusiness.Interface/BugSecurity/DTO/RoleDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugBusiness.Interface.BugSecurity.DTO
+{
+    public class RoleDTO
+    {
+        public long RoleID { get; set; }
+        public long RoleType { get; set; }
+        public string RoleDescription { get; set; }
+    }
+}
---
+++ SAMBUG/BugBusiness.Interface/FarmManagement/DTO/GetPestsPerTreeByBlockResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugBusiness.Interface.FarmManagement.DTO
+{
+    public class GetPestsPerTreeByBlockResult
+    {
+        public double PestsPerTree { get; set; }
+        public string LastTreatment { get; set; }
+    }
+}
