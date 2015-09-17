c308b49157fbac4f35e9d78e6a609860a7f1d1fc
Werner Mostert <werner.mostert1@gmail.com>
2015-09-17 13:49:44 +0200

    Image Processing API started

--- SAMBUG/BugWeb/App_Start/AutoFacConfig.cs
+++ SAMBUG/BugWeb/App_Start/AutoFacConfig.cs
@@ -19,6 +19,8 @@ using System.Web.Http;
 using System.Reflection;
 using BugBusiness.BugScouting;
 using BugBusiness.Interface.BugScouting;
+using BugBusiness.BugIntelligence;
+using BugBusiness.Interface.BugIntelligence;
 
 namespace BugWeb
 {
@@ -43,6 +45,8 @@ namespace BugWeb
 
             builder.RegisterType<DbScouting>().As<IDbScouting>();
             builder.RegisterType<BugScouting>().As<IBugScouting>();
+
+            builder.RegisterType<BugIntelligence>().As<IBugIntelligence>();
             
             
 
---
+++ SAMBUG/BugWeb/Controllers/ApiSpeciesClassificationController.cs
@@ -0,0 +1,38 @@
+﻿using BugBusiness.Interface.BugIntelligence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BugBusiness.Interface.BugIntelligence.DTO;
+
+namespace BugWeb.Controllers
+{
+    [RoutePrefix("api/classification")]
+    public class ApiSpeciesClassificationController : ApiController
+    {
+        private readonly IBugIntelligence _bugIntelligence;
+
+        public ApiSpeciesClassificationController(IBugIntelligence bugIntelligence)
+        {
+            _bugIntelligence = bugIntelligence;
+        }
+
+        [HttpPost]
+        public ClassifyResult Post([FromBody] ClassifyRequest request)
+        {
+           
+            try
+            {
+                return _bugIntelligence.classify(BugBusiness.ExtensionMethods.DataConversion.sbyteToByteArray(request.FieldPicture));
+            }
+            catch (Exception e)
+            {
+                return new ClassifyResult();
+            }
+
+        }
+
+    }
+}
