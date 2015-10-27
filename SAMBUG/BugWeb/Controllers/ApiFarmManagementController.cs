0cf6b52b3a1998e1d228a9aeb077b7895d9b7264
Michelle14 <michelle4swanepoel@gmail.com>
2015-10-27 14:20:14 +0200

    Changes to edmx file

---
+++ SAMBUG/BugWeb/Controllers/ApiFarmManagementController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BugWeb.Controllers
+{
+    public class ApiFarmManagementController : ApiController
+    {
+    }
+}
